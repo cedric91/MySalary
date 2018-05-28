d17def603b26cb02cb3341df78ec2194b36af8ae
cedric91 <dejiar_91@hotmail.com>
2018-05-28 17:10:40 +0800

    update

--- app/src/main/java/mysalary/dj/com/mysalary/Summary.java
+++ app/src/main/java/mysalary/dj/com/mysalary/Summary.java
@@ -6,14 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Summary extends Fragment {
 
+    private Spinner spinner;
     DatabaseHelper db;
 
     @Override
@@ -22,10 +27,31 @@ public class Summary extends Fragment {
         View rootView = inflater.inflate(R.layout.summary, container, false);
 
         TextView monthlySpent = (TextView)rootView.findViewById(R.id.monthlySpending);
+        spinner = (Spinner) rootView.findViewById(R.id.records);
+
         db = new DatabaseHelper(getActivity());
         Cursor cursor = db.readSpending();
+        Cursor readMonthCursor = db.readMonth();
+
+        if(readMonthCursor.moveToFirst()){
+            String[] date = new String[readMonthCursor.getCount()];
+            int i = 0;
+            do {
+                date[i] = (String.valueOf(readMonthCursor.getString(cursor.getColumnIndex("month"))));
+            }while(readMonthCursor.moveToNext());
+
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, Arrays.asList(date));
+
+            // Drop down layout style - list view with radio button
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            // attaching data adapter to spinner
+            spinner.setAdapter(dataAdapter);
+        }
+
         if(cursor.moveToFirst()){
             String[] spent = new String[cursor.getCount()];
+
             int i = 0;
             double totalSpent = 0;
             do {
@@ -37,7 +63,6 @@ public class Summary extends Fragment {
             //monthlySpent.setText(Arrays.toString(spent));
             monthlySpent.setText("Your spending for January is : "+String.valueOf(totalSpent));
         }
-
 
         return rootView;
     }
